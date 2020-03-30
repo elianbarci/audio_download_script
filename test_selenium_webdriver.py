496bee5acc7fc0a907fafaf856aa226f5bad9853
Elian Barci <eli.barci@gmail.com>
2020-03-30 02:56:12 -0300

    Ya descarga los archivos

--- test_selenium_webdriver.py
+++ test_selenium_webdriver.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
+import requests
+
 
 soups = []
 driver = webdriver.Chrome()
@@ -8,7 +10,7 @@ driver.get('http://bbcsfx.acropolis.org.uk/')
 page = 0
 time.sleep(5)
 
-while page < 12:
+while page < 2:
     soups.append(BeautifulSoup(driver.page_source))
     page = page + 1
     driver.get('http://bbcsfx.acropolis.org.uk/?page=' + str(page))
@@ -23,5 +25,12 @@ for soup in soups:
     for tag in tags:
         #links.append('http://bbcsfx.acropolis.org.uk' + tag.get('src')[2:] + "\n")#Slice syntax give me the part of this sequence which begins at index 2 and continues to the end (since no end point was specified after the colon)
         file1.write('http://bbcsfx.acropolis.org.uk' + tag.get('src')[2:] + "\n")
+        r = requests.get('http://bbcsfx.acropolis.org.uk' + tag.get('src')[2:])
+        with open(tag.get('src')[10:], 'wb') as f:
+            f.write(r.content)
+            f.close()
+
 
 file1.close()
+
+
