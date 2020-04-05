dc4460c5db83701d9a49787a82af6758c653d77b
Elian Barci <eli.barci@gmail.com>
2020-04-05 01:03:59 -0300

    Cambios estructurales

--- BBC Audio Download/test_selenium_webdriver.py
+++ BBC Audio Download/test_selenium_webdriver.py
@@ -5,12 +5,14 @@ import requests
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
+import re
 
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
 drive = GoogleDrive(gauth)
 
 fileList = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+
 for file in fileList:
   if(file['title'] == "TestUpload"):
       fileID = file['id']
@@ -27,6 +29,8 @@ while page < 1:
     driver.get('http://bbcsfx.acropolis.org.uk/?page=' + str(page))
     time.sleep(5)
 
+
+
 driver.quit
 file1 = open("wav_links.txt","w")
 filename = ""
@@ -38,15 +42,16 @@ for soup in soups:
     aux_description = 0
     
     for tag in tags:
-        file1.write('http://bbcsfx.acropolis.org.uk' + tag.get('src')[2:] + "\n")
+        filename = description[aux_description].getText()
+        filename = re.sub('[!@#$./\?%*:|"<>]', '', filename)
         print("Se comenzo a descargar el archivo: " + filename)
         r = requests.get('http://bbcsfx.acropolis.org.uk' + tag.get('src')[2:])
-        filename = tag.get('src')[10:]
 
         with open(filename, 'wb') as f:
             f.write(r.content)
             print("Se descargo correctamente el archivo: " + filename)
-
+                    
+        file1.write(file1.write(description[aux_description].getText() + "\n"))            
         file_upload = drive.CreateFile({"mimeType": "audio/*", "parents": [{"kind": "drive#fileLink", "id": fileID}]})
         file_upload.SetContentFile(filename)
         
@@ -54,13 +59,14 @@ for soup in soups:
             file_upload.Upload()
         finally:
             file_upload.content.close()
+
         if file_upload.uploaded: 
             print("Se subio correctamente el archivo: " + filename)
             try:
                 os.remove(filename)
-                print(" El archivo fue eliminado correctamente") 
+                print("El archivo: fue eliminado correctamente") 
             except OSError:
-                print(" El archivo fue eliminado incorrectamente") 
+                print("El archivo fue eliminado incorrectamente") 
                 pass
 
         aux_description = aux_description + 1
